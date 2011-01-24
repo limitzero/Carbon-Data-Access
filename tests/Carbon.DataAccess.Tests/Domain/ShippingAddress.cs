2024992b227c90b5678e787b038100faead34a98
limitzero <limitzero@onebox.com>
2011-01-24 14:21:10 -0600

    updated to VS2010 and added support for nullables

---
+++ tests/Carbon.DataAccess.Tests/Domain/ShippingAddress.cs
@@ -0,0 +1,25 @@
+﻿namespace Carbon.DataAccess.Tests.Domain
+{
+    /// <summary>
+    /// ShippingAddress "is a" Address, but the entity that should 
+    /// be preserved is the the ShippingAddress, not the base value type
+    /// this is why it needs an identifier column.
+    /// </summary>
+    public class ShippingAddress : Address
+    {
+        private int _id;
+
+        public virtual int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public ShippingAddress()
+        { }
+
+       
+    }
+}
